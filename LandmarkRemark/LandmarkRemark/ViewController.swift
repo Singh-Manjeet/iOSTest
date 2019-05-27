068bfb8a91316bcb646757fd4667c2c0b44e28a8
manjeet <manjeet.dhanjal@gmail.com>
2019-05-27 21:02:18 +1000

    Project Initialised

---
+++ LandmarkRemark/LandmarkRemark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkRemark
+//
+//  Created by Manjeet Singh on 27/5/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
