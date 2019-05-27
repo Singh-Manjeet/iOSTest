38b24c51794bc79222ca93ee7aa6a4e220b05f00
manjeet <manjeet.dhanjal@gmail.com>
2019-05-27 21:32:12 +1000

    Added initial scene i.e welcome view controller Structure update Added app launcher class

---
+++ LandmarkRemark/LandmarkRemark/Scenes/Launcher.swift
@@ -0,0 +1,20 @@
+//
+//  Launcher.swift
+//  LandmarkRemark
+//
+//  Created by Manjeet Singh on 27/5/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class Launcher {
+    
+    // MARK: - Static methods
+    static func launch(with window: UIWindow?) {
+        if let navigationController = window?.rootViewController as? UINavigationController,
+            let _ = navigationController.viewControllers.first as? WelcomeViewController {
+        }
+    }
+}
+
